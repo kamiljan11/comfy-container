9f3a6c40003bae9a49af884dc8154e35a55130d3
mountainallservice <mountainallservice@gmail.com>
2026-05-21 23:32:47 +0000

    feat: import all 6 SPS parts in /spirituality/sps route

--- src/routes/spirituality.sps.tsx
+++ src/routes/spirituality.sps.tsx
@@ -3,10 +3,14 @@ import SpiritualityLayout from '../components/SpiritualityLayout'
 import MarkdownContent from '../components/MarkdownContent'
 import part1 from '../content/spirituality/sps-part-1.md?raw'
 import part2 from '../content/spirituality/sps-part-2.md?raw'
+import part3 from '../content/spirituality/sps-part-3.md?raw'
+import part4 from '../content/spirituality/sps-part-4.md?raw'
+import part5 from '../content/spirituality/sps-part-5.md?raw'
+import part6 from '../content/spirituality/sps-part-6.md?raw'
 
 export const Route = createFileRoute('/spirituality/sps')({ component: SpsPage })
 
-const sps = [part1, part2].join('\n\n')
+const sps = [part1, part2, part3, part4, part5, part6].join('\n\n')
 
 function SpsPage() {
   return (
