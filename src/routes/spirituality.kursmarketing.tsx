4cb196a68559c3bfd79e876be96e312aa3fc2529
mountainallservice <mountainallservice@gmail.com>
2026-05-24 16:41:29 +0000

    Remove spirituality subpage

--- src/routes/spirituality.kursmarketing.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/spirituality/kursmarketing')({
-  beforeLoad: () => {
-    throw redirect({ to: '/spirituality/marketing-training' })
-  },
-  component: () => null,
-})
